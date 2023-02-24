ee10af3a7a3de0d63d700c26a7afd9446fc2c6e0
MackyClap <martimac2320@gmail.com>
2023-02-23 21:24:11 -0500

    spaced first row of grid items according to landing page mockup

--- src/Pages/UserLandingPage.js
+++ src/Pages/UserLandingPage.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
+import Grid from '@mui/material/Unstable_Grid2';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -16,21 +16,22 @@ export default function UserLandingPage() {
   return (
     <Grid 
       container 
-      spacing={2}
+      rowSpacing={10}
+      columnSpacing={3}
       pt={6}
-      >
-  <Grid item xs={8}>
-    <Item>xs=8</Item>
-  </Grid>
-  <Grid item xs={4}>
-    <Item>xs=4</Item>
-  </Grid>
-  <Grid item xs={4}>
-    <Item>xs=4</Item>
-  </Grid>
-  <Grid item xs={8}>
-    <Item>xs=8</Item>
-  </Grid>
-</Grid>
+    >
+      <Grid item xs={2}>
+        <Item>Current Date</Item>
+      </Grid>
+      <Grid item xs={2}>
+        <Item>xs=4</Item>
+      </Grid>
+      <Grid item xs={6}>
+        <Item>xs=4</Item>
+      </Grid>
+      <Grid item xs={2}>
+        <Item>xs=8</Item>
+      </Grid>
+    </Grid>
   );
 }
