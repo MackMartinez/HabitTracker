79c1fe316b9872df0f49b2d39dc533b2c50aa211
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-02-23 22:33:23 -0500

    Remove dead code and add prop to pass down habit

--- src/Components/Habit/HabitsListItem.jsx
+++ src/Components/Habit/HabitsListItem.jsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
 import ListItem from '@mui/material/ListItem';
-import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
 import IconButton from '@mui/material/IconButton';
 
 
-export default function HabitListItem() {
+export default function HabitListItem(props) {
 
   const [selectedIndex, setSelectedIndex] = React.useState(1);
 
@@ -22,7 +20,7 @@ export default function HabitListItem() {
         selected={selectedIndex === 0}
         onClick={(event) => handleListItemClick(event, 0)}
       >
-        <ListItemText primary ="Habit"/>
+        <ListItemText primary ={props.habitListItem}/>
       </ListItemButton>
       
       <IconButton>
