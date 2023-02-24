81ce9ca8f57748b86252c39229f60ab17a964d10
MackyClap <martimac2320@gmail.com>
2023-02-23 20:59:19 -0500

    add MUI gruid

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import * as React from 'react';
 import ButtonAppBar from './Components/Appbar';
+import UserLandingPage from './Pages/UserLandingPage';
 
 
 function App() {
   return (
     <React.Fragment>
       <ButtonAppBar />
+      <UserLandingPage />
     </React.Fragment>
   );
 }
