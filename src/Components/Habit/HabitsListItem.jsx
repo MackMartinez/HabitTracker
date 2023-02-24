6a35ccc2da517b170cbadc13b9fd7c5fe5ddba06
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-02-23 22:20:06 -0500

    Add remove button to habit item

--- src/Components/Habit/HabitsListItem.jsx
+++ src/Components/Habit/HabitsListItem.jsx
@@ -4,27 +4,30 @@ import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import RemoveCircleIcon from '@mui/icons-material/RemoveCircle';
+import IconButton from '@mui/material/IconButton';
 
-const habits = ["piano", "running", "swimming"];
-
-let habitComponent = habits.map((habit) => {
-  return (
-    <ListItemButton>
-      <ListItemText primary ={habit}/>
-    </ListItemButton>
-  )
-})
 
 export default function HabitListItem() {
+
   const [selectedIndex, setSelectedIndex] = React.useState(1);
 
   const handleListItemClick = (event, index) => {
     setSelectedIndex(index);
   };
-  
+
   return(
-   <div>
-     {habitComponent}
-   </div>  
+    <ListItem>
+      <ListItemButton
+        selected={selectedIndex === 0}
+        onClick={(event) => handleListItemClick(event, 0)}
+      >
+        <ListItemText primary ="Habit"/>
+      </ListItemButton>
+      
+      <IconButton>
+        <RemoveCircleIcon /> 
+      </IconButton>
+    </ListItem>
   )
 }
