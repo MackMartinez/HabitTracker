fa94212cd7003c26a1d936f4b7b8830a1833150e
MackyClap <martimac2320@gmail.com>
2023-02-25 13:22:03 -0500

    create basic registration form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import * as React from 'react';
 import ButtonAppBar from './Components/Appbar';
+import Register from './Pages/Register';
+
 
 
 function App() {
   return (
     <React.Fragment>
       <ButtonAppBar />
+      <Register />
     </React.Fragment>
   );
 }
