d044ba7768ec51579912d160929bfef4105687dd
MackyClap <martimac2320@gmail.com>
2023-02-25 12:55:31 -0500

    add remember me checkbox and sign in button

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,17 +1,19 @@
 import React from "react";
-import { Grid, Paper, Avatar, TextField } from "@mui/material";
+import { Grid, Paper, Avatar, TextField, Checkbox, FormGroup, FormControlLabel, Button } from "@mui/material";
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 
 export default function Login () {
 
   const paperStyle = {
     padding: 20,
-    height: '70vh',
-    width: 280,
+    height: 'auto',
+    width: '50vh',
     margin: '70px auto'
   }
 
   const avatarStyle = {backgroundColor: 'blue'}
+
+  const textStyle = {margin:'8px 0px'}
 
   return (
     <Grid>
@@ -19,20 +21,31 @@ export default function Login () {
         <Grid align='center'>
           <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
           <h2>Login</h2>
-          <TextField 
-            id="outlined-basic" 
-            label="Email" 
-            variant="outlined"
-            placeholder="user@email.com" 
-          />
-          <TextField 
-            id="outlined-basic" 
-            label="Password" 
-            variant="outlined"
-            type="password"
-            //Must be 6 characters minimum
-            placeholder="xxxxxx" 
-          />
+          <FormGroup>
+            <TextField 
+              id="outlined-basic" 
+              label="Email" 
+              variant="outlined"
+              placeholder="user@email.com" 
+              style={textStyle}
+            />
+            <TextField 
+              id="outlined-basic" 
+              label="Password" 
+              variant="outlined"
+              type="password"
+              //Must be 6 characters minimum
+              placeholder="xxxxxx" 
+              style={textStyle}
+            />
+            <FormControlLabel control={<Checkbox />} label="Remember Me" />
+            <Button type="submit" color="primary" variant="contained">
+              Sign In
+            </Button>
+              //forgot passord?
+              //Don't have an account? Register here
+
+          </FormGroup>
         </Grid>
       </Paper>
     </Grid>
