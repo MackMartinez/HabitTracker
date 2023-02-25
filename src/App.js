85e03deacea1f3d8b97976df0d7b73cc5bc9d24b
MackyClap <martimac2320@gmail.com>
2023-02-25 14:18:46 -0500

    create initial layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import * as React from 'react';
 import ButtonAppBar from './Components/Appbar';
+import CreateHabit from './Components/CreateHabit';
 
 
 function App() {
   return (
     <React.Fragment>
       <ButtonAppBar />
+      <CreateHabit />
     </React.Fragment>
   );
 }
---
+++ src/Components/CreateHabit.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+
+export default function CreateHabit () {
+
+  const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+  return (
+    <Grid 
+      container 
+      rowSpacing={2}
+      columnSpacing={3}
+      pt={12}
+    >
+      <Grid item xs={2}>
+        <Item>Current Date</Item>
+      </Grid>
+      <Grid item xs={10}>
+        <h1>Create Habit</h1>
+      </Grid>
+      <Grid item xs={2}>
+        <Item style={{minHeight: "1000px", backgroundColor: "inherit"}}>
+          Habit Summary
+        </Item>
+      </Grid>
+      <Grid item xs={5}>
+        <Item style={{minHeight: "1000px"}}>
+          <h2>Pros</h2>
+        </Item>
+      </Grid>
+      <Grid item xs={5}>
+        <Item style={{minHeight: "1000px"}}>
+          <h2>Cons</h2>
+        </Item>
+      </Grid>
+    </Grid>
+  )
+}
