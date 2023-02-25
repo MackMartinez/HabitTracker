99153320268bf1ddb34c709ca34ece8d30aa3d5f
MackyClap <martimac2320@gmail.com>
2023-02-25 12:20:57 -0500

    add basic Grid component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import * as React from 'react';
 import ButtonAppBar from './Components/Appbar';
+import Login from './Pages/Login';
 import UserLandingPage from './Pages/UserLandingPage';
 
 
 function App() {
   return (
     <React.Fragment>
-      <ButtonAppBar />
-      <UserLandingPage />
+      {/* <ButtonAppBar /> */}
+      <Login />
+      {/* <UserLandingPage /> */}
     </React.Fragment>
   );
 }
---
+++ src/Pages/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Grid, Paper, Avatar } from "@mui/material";
+import { LockOutlinedIcon } from "@mui/icons-material";
+
+export default function Login () {
+  return (
+    <Grid>
+      <Paper>
+        <h2>Hello</h2>
+      </Paper>
+    </Grid>
+
+  );
+}
