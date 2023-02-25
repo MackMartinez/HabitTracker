9ae04a21814f155dbba8eeddf365c5cde351bdff
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-02-25 18:37:28 -0500

    Update to reference HabitPageLayout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import * as React from 'react';
 import ButtonAppBar from './Components/Appbar';
-import Habit from './Components/Habit/HabitCard'
+import HabitPageLayout from './Components/Habit/HabitPageLayout';
 
 
 function App() {
   return (
     <React.Fragment>
       <ButtonAppBar />
-      <Habit/>
+      <HabitPageLayout/>
+     
     </React.Fragment>
   );
 }
