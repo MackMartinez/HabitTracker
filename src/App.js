75d577e9d41f3ddf5c02e4d485896208783056be
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-02-25 17:45:28 -0500

    Remove dead code

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import * as React from 'react';
 import ButtonAppBar from './Components/Appbar';
+import Habit from './Components/Habit/HabitCard'
 
 
 function App() {
   return (
     <React.Fragment>
       <ButtonAppBar />
+      <Habit/>
     </React.Fragment>
   );
 }
