eda5f1a5d0c2d7b44bde43c9a9a2ecf66994c334
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-02-26 18:59:45 -0500

    Remove console log from map

--- src/Components/Habit/HabitList.jsx
+++ src/Components/Habit/HabitList.jsx
@@ -4,7 +4,7 @@ import HabitListItem from "./HabitsListItem";
 const habits = ['Piano', 'Swimming', 'Jogging'];
 
 let habitsList = habits.map((habit, index) => {
-  console.log(habit);
+  
   return(
     <HabitListItem key ={index} habitListItem={habit}/>
   )
