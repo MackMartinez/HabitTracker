ba9f37fab1d7e7d34ec1aad9ec65784ec2943d63
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-02-26 18:58:30 -0500

    Update button to use add habit component

--- src/Components/Habit/HabitCard.jsx
+++ src/Components/Habit/HabitCard.jsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
+import AddHabit from './AddHabit';
 import HabitList from './HabitList';
+
 
 
 export default function BasicCard() {
@@ -14,7 +15,7 @@ export default function BasicCard() {
          <HabitList/>
         </CardContent>
         <CardActions>
-          <Button size="small">Add Habit</Button>
+          <AddHabit/>
         </CardActions>
       </Card>
     </>
