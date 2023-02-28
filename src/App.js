646f597f581f2f3f9f44d7555dfd1b9de9c5f5f5
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-02-27 21:34:09 -0500

    Add user habit page to react router

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import * as React from 'react';
 import Register from './Pages/Register';
 import Login from './Pages/Login';
 import UserLandingPage from './Pages/UserLandingPage';
+import HabitPageLayout from './Pages/Habits';
 
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
@@ -15,6 +16,7 @@ function App() {
       <Route path="/register" element={<Register/>}/>
       {/* This should be /:user to render specific user logged in  */}
       <Route path="/user" element={<UserLandingPage/>}/>
+      <Route path="/habit" element={<HabitPageLayout/>}/>
     </Routes>
     </BrowserRouter>
   );
