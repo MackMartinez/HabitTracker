ed71fcfef37059742d47ff360712156318461731
MackyClap <martimac2320@gmail.com>
2023-03-01 21:36:00 -0500

    show chart on user/habit page with sample data

--- src/Pages/Habits.jsx
+++ src/Pages/Habits.jsx
@@ -8,6 +8,7 @@ import Paper from '@mui/material/Paper';
 import ButtonAppBar from '../Components/Appbar';
 import Chart from '@toast-ui/chart';
 import { BarChart } from '@toast-ui/chart';
+import '@toast-ui/chart/dist/toastui-chart.min.css';
 
 
 
@@ -26,6 +27,29 @@ export default function HabitPageLayout () {
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedValue(event.target.value);
   };
+
+  const el = document.getElementById('chart');
+
+  const data = {
+  categories: ['Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+  series: [
+    {
+      name: 'Budget',
+      data: [5000, 3000, 5000, 7000, 6000, 4000, 1000],
+    },
+    {
+      name: 'Income',
+      data: [8000, 4000, 7000, 2000, 6000, 3000, 5000],
+    },
+  ],
+};
+
+const options = {
+  chart: { width: 700, height: 400 },
+};
+
+const barChartOne = Chart.barChart({ el, data, options });
+// // const chart = new BarChart({ el, data, options }); // Second way
 
   return(
       <>
@@ -87,6 +111,7 @@ export default function HabitPageLayout () {
           </Grid>
           <Grid xs={7}>
             <h2>Graph showing your weekly progress</h2>
+            <div id='chart'></div>
           </Grid>
 
         </Grid>
