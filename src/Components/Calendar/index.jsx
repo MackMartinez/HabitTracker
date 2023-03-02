c158d6d93406fcae07851c67210ba93643f3c562
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-03-01 21:01:37 -0500

    Add events to calendar

--- src/Components/Calendar/index.jsx
+++ src/Components/Calendar/index.jsx
@@ -3,10 +3,44 @@ import Calendar from '@toast-ui/react-calendar';
 import '@toast-ui/calendar/dist/toastui-calendar.min.css';
 
 
-export default function schedule(props) {
-  return(
+const calendars = [{ id: 'cal1', name: 'Personal' }];
+  const initialEvents = [
+    {
+      id: '1',
+      calendarId: 'cal1',
+      title: 'Lunch',
+      category: 'time',
+      start: '2023-03-01T12:00:00',
+      end: '2023-03-01T13:30:00',
+    },
+    {
+      id: '2',
+      calendarId: 'cal1',
+      title: 'Coffee Break',
+      category: 'time',
+      start: '2023-02-28T15:00:00',
+      end: '2023-02-28T15:30:00',
+    },
+  ];
+
+  const onAfterRenderEvent = (event) => {
+    console.log(event.title);
+  };
+
+ export default function schedule(props){
+   return (
     <div>
-      <Calendar view={props.view}/>
+      <Calendar
+        height="900px"
+        view={props.view}
+        month={{
+          dayNames: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+          visibleWeeksCount: 3,
+        }}
+        calendars={calendars}
+        events={initialEvents}
+        onAfterRenderEvent={onAfterRenderEvent}
+      />
     </div>
-  )
+  );
 }
