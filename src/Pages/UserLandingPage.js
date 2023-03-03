fb302ed4c69445dab14e08bd04df752e322f318c
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-03-02 20:25:32 -0500

    Remove Calendar Component and dependencies then add HabitCalendar component"

--- src/Pages/UserLandingPage.js
+++ src/Pages/UserLandingPage.js
@@ -6,10 +6,9 @@ import Radio from '@mui/material/Radio';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import ButtonAppBar from '../Components/Appbar';
 import AddHabitButton from '../Components/Habit/AddHabitButton';
-import Calendar from '@toast-ui/react-calendar';
-import '@toast-ui/calendar/dist/toastui-calendar.min.css';
 import HabitList from '../Components/Habit/HabitList';
-import { Schedule } from "@mui/icons-material";
+import HabitCalendar from "../Components/Calendar";
+
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -19,38 +18,6 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
-const calendars = [{ useDetailPopup: true }]; // from the calendar index file
-
-const initialEvents = [
-  {
-    id: '1',
-    calendarId: 'cal1',
-    title: 'Lunch',
-    body: 'Going to eat a cheeseburger',
-    category: 'time',                       // Determines where in the calendar the habit is displayed (Time, allday or milestone)
-    start: '2023-03-01T12:00:00',
-    end: '2023-03-01T13:30:00',
-    state: null,                            //Removes the 'busy' tag from the popout menu
-    attendees: null,                        //Removes the person icon from the popout menu
-    isPrivate: false,
-    backgroundColor: "#1976d2",
-    color: "white",
-  },
-  {
-    id: '2',
-    calendarId: 'cal1',
-    title: 'Coffee Break',
-    category: 'time',
-    start: '2023-02-28T15:00:00',
-    end: '2023-02-28T15:30:00',
-    state: null,
-    attendees: null,
-    isPrivate: false,
-    backgroundColor: "#1976d2",
-    color: "white"
-  },
-];
-
 export default function UserLandingPage() {
   const [calendarView, setCalendarView] = React.useState('week');
 
@@ -58,13 +25,6 @@ export default function UserLandingPage() {
     setCalendarView(event.target.value);
   };
 
-  const calref = React.useRef(null);
-
-  const handlenext = () => {
-    const currentCal = calref.current.getInstance();
-    currentCal.next();
-    // calref.current.next()
-  }
 
   return (
     <>
@@ -131,31 +91,10 @@ export default function UserLandingPage() {
       </Grid>
       <Grid item xs={10}>
         <Item>
-          Calendar
-          <Grid>
-            <button>Prev</button>
-            <button onClick={handlenext}>Next</button>
-            <Calendar
-              ref={calref}
-              height="900px"
-              view={calendarView}
-              month={{
-                dayNames: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
-                visibleWeeksCount: 3,
-              }}
-              calendars={calendars}
-              events={initialEvents}
-              useDetailPopup={true}
-              useFormPopup={true}
-        
-      />
-          </Grid>
+          <HabitCalendar view={calendarView}/>
         </Item>
       </Grid>
     </Grid>
   </>
   );
 }
-
-
-<Schedule view="something"/>
