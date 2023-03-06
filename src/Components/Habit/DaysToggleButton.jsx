b20244b8eab6ee2874b914217d7c524472c32b58
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-03-05 23:36:39 -0500

    Add key to toggle button and add set habit to handle change function

--- src/Components/Habit/DaysToggleButton.jsx
+++ src/Components/Habit/DaysToggleButton.jsx
@@ -4,21 +4,23 @@ import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import { useState } from 'react';
 import { Typography } from '@mui/material';
 
-export default function DaysToggleButtons() {
-  const [daySelected, setDaySelected] = useState([]);
+export default function DaysToggleButtons(props) {
+  const [daySelected, setDaySelected] = useState("");
 
   const daysButtonStyle = {maxWidth: '30px', maxHeight: '30px', minWidth: '30px', minHeight: '30px', margin: '20px 20px 20px 20px'}
 
   const daysArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 
-  const handleChange = (event,day) => {
-    setDaySelected(day);
+  const handleChange = ( day) => {
+    setDaySelected(day); // This passes a stale array, so the value is always missing the last day selected
+    props.setHabit(prev => ({...prev, days: day})); // Added to ensure that the habit state is updated for the days selected
   };
 
-  const eachDayButton = daysArray.map(day => {
+  const eachDayButton = daysArray.map((day, index) => {
     return(
       <ToggleButton
         value={day}
+        key={index}
       >
         <Typography> {day} </Typography>
       </ToggleButton>
