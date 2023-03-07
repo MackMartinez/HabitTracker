1cc98a9ec541bd444985da3ec65a7388d5586e19
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-03-07 14:09:11 -0500

    Add function to add days to the start day of the events passed on

--- src/helpers/events.js
+++ src/helpers/events.js
@@ -11,15 +11,26 @@ let weekObject = {
 }
 
 
+const addDays = (startDate, num) => {
+  let someDate = new Date(startDate)
+  console.log("someDate", someDate)
+  let numberOfDaysToAdd = num;
+  let result = someDate.setDate(someDate.getDate() + numberOfDaysToAdd);
+  let output = new Date(result)
+  console.log("result",result);
+  return output;
+}
 
-const generateEvents = (habit, sunday) => {
 
+const generateEvents = (habit) => { // Use sunday as the reference point to change the day
+
+  // Template event object to be created
   let event = {
     title: habit.title,
     body: habit.details,
     category: "time",
-    start: `${habit.startDate}T${habit.startTime}:00`,
-    end: `${habit.startDate}T${habit.startTime}:00`,
+    start: habit.startDate,
+    end: habit.startDate,
     state: null,
     attendees: null,
     isPrivate: false,
@@ -30,9 +41,17 @@ const generateEvents = (habit, sunday) => {
   
   let eventsGenerated = [];
 
-  habit.days.forEach(day => console.log (day));
+  habit.days.forEach(day =>
+    {
+     
+      let newDay = addDays("2023-03-05", weekObject[day])
 
-  return event;
+      let updatedEvent = {...event, start: newDay, end: newDay}
+      eventsGenerated.push(updatedEvent);
+    });
+
+    console.log(eventsGenerated);
+  return eventsGenerated;
 
 }
 
