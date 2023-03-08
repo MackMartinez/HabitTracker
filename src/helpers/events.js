6391121e5a565145f653879641b71ac82e3bafc7
Jevaughn Williams <jevaughn.williamsx@gmail.com>
2023-03-07 22:19:54 -0500

    Fix bug causing improper dates and time used when generating events

--- src/helpers/events.js
+++ src/helpers/events.js
@@ -1,4 +1,4 @@
-
+import moment from "moment";
 
 let weekObject = {
   "Sunday": 0,
@@ -11,27 +11,31 @@ let weekObject = {
 }
 
 
-const addDays = (startDate, num) => { // Adds days to the date passed in
-  let someDate = new Date(startDate)
-  console.log("someDate", someDate)
-  let numberOfDaysToAdd = num;
-  let result = someDate.setDate(someDate.getDate() + numberOfDaysToAdd);
-  let output = new Date(result)
-  console.log("result",result);
-  return output;
+const addDays = (startDate, endDate, num) => { // Adds days to the date passed in
+  // Event Start
+  let eventStart = moment(startDate);
+  let eventStartResult = eventStart.add(num, "days");
+  // Event End
+  let eventEnd = moment(endDate);
+  let eventEndResult = eventEnd.add(num, "days");
+
+  console.log("date", eventEnd.format());
+ 
+  return [eventStartResult.format(), eventEndResult.format()];
 }
 
-const calculateDuration = (start, end) => {
-  let startDate = new Date(start);
-  let endDate = new Date(end);
-  let weeks = 4
-  return weeks;
-}
+// const calculateDuration = (start, end) => {
+//   let startDate = new Date(start);
+//   let endDate = new Date(end);
+//   let weeks = 4
+//   return weeks;
+// }
 
 
 const generateEvents = (habit, sunday) => { // Use sunday as the reference point to change the day
 
-  let sundayConverted = `${sunday.d.getFullYear()}-0${sunday.d.getMonth() + 1}-0${sunday.d.getDate()}`;  // Moment JS could probably format better
+  let SundayEventStart = `${sunday.d.getFullYear()}-0${sunday.d.getMonth() + 1}-0${sunday.d.getDate()}T${habit.startTime}:00`;  // Moment JS could probably format better
+  let SundayEventEnd = `${sunday.d.getFullYear()}-0${sunday.d.getMonth() + 1}-0${sunday.d.getDate()}T${habit.endTime}:00`;  // Moment JS could probably format better
   // Template event object to be created
   let event = {
     title: habit.title,
@@ -50,18 +54,24 @@ const generateEvents = (habit, sunday) => { // Use sunday as the reference point
   let eventsGenerated = [];
 
   // let duration = calculateDuration(habit.start, habit.Date)
+  
+  // let startDate = rangeStart;  // The day starting the range for the repeat
 
-  habit.days.forEach(day =>
-    {
-     
-      let newDay = addDays("2023-03-05", weekObject[day])
+  // for (let i = 0; i < duration; i ++ ) {
+    
+    habit.days.forEach(day => {
+      let [newStartDay, newEndDay] = addDays(SundayEventStart, SundayEventEnd, weekObject[day])
+      
+      let updatedEvent = {...event, start: newStartDay, end: newEndDay}
 
-      let updatedEvent = {...event, start: newDay, end: newDay}
+      // startDate = addDays(startDate, 7)
       eventsGenerated.push(updatedEvent);
     });
+  // }
+
 
     console.log(eventsGenerated);
-    console.log("Sunday", sundayConverted)
+    // console.log("Sunday", rangeStart)
   return eventsGenerated;
 
 }
