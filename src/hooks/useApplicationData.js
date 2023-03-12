552ebe46fe89e974feae150216910cda9091fef8
MackyClap <martimac2320@gmail.com>
2023-03-12 11:05:36 -0400

    update to get events count query from db

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -6,14 +6,16 @@ export default function useApplicationData() {
   
   const [ state, setState ] = useState({
     habits: [],
-    events: []
+    events: [],
+    eventsCount:[]
   });
   
    // Use effect to make axios call and get habit data
   useEffect(() => {
     const routes = {
       getHabits: "http://localhost:8080/habit",
-      getEvents: "http://localhost:8080/habit/events"
+      getEvents: "http://localhost:8080/habit/events",
+      getEventsCount: "http://localhost:8080/habit/events/count"
   };
 
     Promise.all([
@@ -22,9 +24,12 @@ export default function useApplicationData() {
       }),
       axios.get(routes.getEvents, {
         withCredentials: true
-  })
+      }),
+      axios.get(routes.getEventsCount, {
+        withCredentials: true
+      })
     ]).then((all) => {
-      setState(prev => ({...prev, habits: all[0].data, events: all[1].data}))
+      setState(prev => ({...prev, habits: all[0].data, events: all[1].data, eventsCount: all[2].data}))
       // console.log(state.events)
     });
   },[])
