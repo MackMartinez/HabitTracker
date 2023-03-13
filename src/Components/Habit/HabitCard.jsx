9f0f750b11aa8133729816c12bd9f0e67e1d7e9b
MackyClap <martimac2320@gmail.com>
2023-03-13 11:46:41 -0400

    remove unused import

--- src/Components/Habit/HabitCard.jsx
+++ src/Components/Habit/HabitCard.jsx
@@ -5,7 +5,6 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import useApplicationData from '../../hooks/useApplicationData';
 import HabitTabPanel from './Habittab';
 
 
